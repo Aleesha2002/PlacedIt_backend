74926e5b215b95efe46301e9d6052534e00f47b7
Aleesha2002 <aleesha.lalit@gmail.com>
2024-07-08 04:48:16 +0530

    backend

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// mongodb
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // create db
+    const db = client.db("mernJobPortal");
+    const jobsCollections = db.collection("demoJobs");
+
+    // post a job
+    app.post("/post-job", async (req, res) => {
+      const body = req.body;
+      body.createAt = new Date();
+      console.log(body);
+      const result = await jobsCollections.insertOne(body);
+      if (result.insertedId) {
+        return res.status(200).send(result);
+      } else {
+        return res.status(404).send({
+          message: "can not insert! try again",
+          status: false,
+        });
+      }
+    });
+
+    // get all jobs
+    app.get("/all-jobs", async (req, res) => {
+      const jobs = await jobsCollections.find({}).toArray();
+      res.send(jobs);
+    });
+
+    // get jobs by email-id
+    app.get("/myJobs/:email", async (req, res) => {
+      //console.log(req.params.email);
+      const jobs = await jobsCollections
+        .find({ postedBy: req.params.email })
+        .toArray();
+      res.send(jobs);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`port running on ${port}`);
+});
